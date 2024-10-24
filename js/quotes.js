df86708a8268c2e0818f33725ed50ff03be996dc
Harrison Jansen van Beek <harrisonjansenva@u.boisestate.edu>
2024-10-24 11:12:35 -0600

    added functionality to retrieve quotes

--- js/quotes.js
+++ js/quotes.js
@@ -11,11 +11,24 @@ async function handleButtonRetrieveProgrammingQuotes()
     console.log(`selected_category         = ${selected_category}`);
 
     // TODO: get value from the number of quotes dropdown
-    let selected_number_of_quotes = -1
+    let selected_number_of_quotes = document.getElementById("id_number_of_quotes").value;
 
     await getAndDisplayAllQuotes(selected_category, selected_number_of_quotes);
 
     console.log('handleButtonRetrieveProgrammingQuotes - END');
+}
+
+function displayQuotes(listOfProgrammingQuotes) {
+    console.log(`displayQuotes - START`)
+    div_list_of_programming_quotes.innerHTML="<ul>"
+    for (const quote of listOfProgrammingQuotes) {
+        console.log({quote});
+        div_list_of_programming_quotes.innerHTML+=`
+        <li>${quote.body} - ${quote.authors}</li>`;
+
+    }
+     div_list_of_programming_quotes.innerHTML+="</ul>";
+
 }
 
 async function getAndDisplayAllQuotes(category, count)
@@ -23,12 +36,31 @@ async function getAndDisplayAllQuotes(category, count)
     console.log('getAndDisplayAllQuotes - START');
 
     // TODO: update the API URL to include the values of the category and count
-    const API_URL = ``;
+    const API_URL = `https://cs208-programming-quotes-ff89e28d2421.herokuapp.com/api/quotes?category=${category}&count=${count}`;
 
     div_list_of_programming_quotes.innerHTML = "Calling the API to get the list of quotes...";
 
     try
     {
+       let response = await fetch(API_URL);
+       console.log(`response.status = ${response.status}`);
+       console.log(`response.body = ${response.statusText}`);
+       console.log(`response.ok = ${response.ok}`);
+
+       if (response.ok)
+       {
+           div_list_of_programming_quotes.innerHTML = "Successfully retrieved quotes.";
+           const listOfProgrammingQuotes = await response.json();      //if we don't put await it won't load in the correct format
+           console.log(listOfProgrammingQuotes);
+           displayQuotes(listOfProgrammingQuotes);
+           // div_list_of_programming_quotes.innerHTML = response.body;
+
+       }
+       else
+       {
+           div_list_of_programming_quotes.innerHTML = "<p class='failure'>Error: the quotes could not be found.</p>";
+       }
+
         // TODO: use the fetch API to call the API_URL
 
         // TODO: check if the response is OK and then process the JSON response and display the quotes in the div_list_of_programming_quotes.innerHTML
